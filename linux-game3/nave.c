e3a0ae4d5e4810f8fb1107a51b924df959073d96
Lucas.Barros <lucas.am.barros.03@gmail.com>
2023-06-17 00:52:16 -0300

    first commit

---
+++ linux-game3/nave.c
@@ -0,0 +1,71 @@
+//
+// Demo: Animacao controlada pelo teclado
+//       Movendo uma "nave" pelo teclado (a,s,d,w)
+// 
+// By FOsorio 2023
+// LINUX Version
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>   // Funcao usleep
+#include <curses.h>   // Funcoes de tela com clear (clrscr)
+
+#define SPEED  10000
+
+int Cols;
+int Lins;
+
+void clrscr(void)
+{
+     clear();
+     refresh();
+}
+
+int main()
+{
+    int i;
+    char tecla;
+
+    Cols=0;
+    Lins=0;
+
+    initscr();
+    cbreak();
+    noecho();
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    curs_set (0);  // Hide Cursor 
+    clrscr();
+    while (true) 
+    {
+        for (i=0; i <= Lins; i++)
+        {
+            printf("\n\r");
+        }
+        for (i=0; i <= Cols; i++)
+        {
+            printf(" ");
+        }
+        printf("(*)\n\r");   // Importante colocar o \n\r após desenho da linha
+
+        usleep(SPEED);
+        clrscr();
+
+        tecla = getch();
+        if (tecla == 'x')
+          break;
+        if (tecla == 'w')    // Up
+          Lins--;
+        if (tecla == 's')    // Down
+          Lins++;
+        if (tecla == 'a')    // Left
+          Cols--;
+        if (tecla == 'd')    // Right
+          Cols++;
+
+    }
+    endwin();
+    return 0; 
+}
+
