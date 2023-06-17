e50ba71909687e05d32c186f88a455cbd033019e
NinjaPro93 <ninjapro93lu@gmail.com>
2023-06-17 14:50:23 -0300

    comecei a tentar movimentar o pacman e a usar a curses

--- try1.c
+++ try1.c
@@ -3,25 +3,30 @@
 #include <unistd.h>   // Funcao usleep
 #include <curses.h>   // Funcoes de tela com clear (clrscr)
 
-#define SPEED  10000
+#define SPEED  100000
 
-#define ROWS  15
+#define ROWS  10
 #define COLS  50
 
 char tab[ROWS][COLS];
 
+int posX = 1;
+int posY = 1;
+
 void init_tab(void){
     for(int i=0; i<ROWS; i++){
         for(int j=0; j<COLS; j++){
+            tab[i][j] = '.';
 
             if(i == 0 || i == ROWS-1){
                 tab[i][j] = '#';
             }
-            else if(j == 0 || j == COLS-1){
+            if(j == 0 || j == COLS-1){
                 tab[i][j] = '#';
             }
-            else{
-                tab[i][j] = '.';
+
+            if(i == posY && j == posX){
+                tab[i][j] = 'C';
             }
         }
     }
@@ -37,7 +42,39 @@ void print_tab(void){
     }
 }
 
+void move_pacman(){
+    tab[posY][posX] = '.';
+    
+    posX += 1;
+    tab[posY][posX] = 'C';
+}
+
 void main(void){
+
+    //NÃO TO ENTENDENDO LEGAL
+    curs_set (0);  // Hide Cursor 
+
+    initscr();
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    noecho();
+
+    clear();
+    refresh();
+    //ATÉ AQUI
+
     init_tab();
     print_tab();
+
+    while(posX < COLS){
+
+        clear();
+        //refresh();
+        usleep(SPEED);
+
+        move_pacman();
+        print_tab();
+    }
+
+    endwin();
 }
