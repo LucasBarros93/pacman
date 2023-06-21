03b568220176c9ad1dc6428558776fd14f797bd5
NinjaPro93 <ninjapro93lu@gmail.com>
2023-06-20 23:21:01 -0300

    V2 da readMap

--- readMap.c
+++ readMap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void main(void){
     FILE *fp;
@@ -10,43 +11,22 @@ void main(void){
     if( !fp ) perror("map.txt"),exit(1);
 
     fseek( fp , 0L , SEEK_END);
-    lSize = ftell( fp );
     rewind( fp );
 
-    /* allocate memory for entire content */
-    buffer = calloc( 1, lSize+1 );
-    if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
-
-    /* copy the file into the buffer */
-    if( 1!=fread( buffer , lSize, 1 , fp) )
-    fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
-
-    /* do your work here, buffer is a string contains the whole text */
-
-    char tab[31][29];
-    int j=0, k=0;
-
-    for(int i=0; i<lSize; i++){
-        if(buffer[i] == '\n'){
-            tab[j][k] = ' ';
-            j++;
-            k=0;
-            continue;
-        }
-        tab[j][k] = buffer[i];
-        k++;
-        
-
+    char tab[31][28];
+    char ch;
+    int i = 0, j = 0;
+    while (fgets(tab[i], 30, fp)){
+        i++;
     }
+    printf("\n");
 
     for(int i=0; i<31; i++){
-        for(int j=0; j<29; j++){
+        for(int j=0; j<28; j++){
             printf("%c", tab[i][j]);
         }
-
         printf("\n\r");
     }
 
     fclose(fp);
-    free(buffer);
 }
